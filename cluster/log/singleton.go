6c91cd0ec98d2fffe5b68a22844848812e1d1fba
werben <werben@aliyun.com>
2023-09-20 17:01:18 +0800

    Optimize code.

--- cluster/log/singleton.go
+++ cluster/log/singleton.go
@@ -8,60 +8,69 @@ import (
 	"sync"
 )
 
-var singleton *Logger
-var mu sync.Mutex
+var defaultLogger *Logger // Singleton instance of the logger.
+var mu sync.Mutex         // Mutex for ensuring thread safety when initializing the logger.
 
+// Init initializes the logger with the provided options.
 func Init(opt *Options) {
 	mu.Lock()
 	defer mu.Unlock()
-	singleton = New(opt)
+	defaultLogger = New(opt)
 }
 
+// Default returns the default logger instance.
 func Default() *slog.Logger {
-	return singleton.Logger
+	return defaultLogger.Logger
 }
 
+// Writer returns the writer associated with the logger.
 func Writer() io.Writer {
-	if singleton != nil {
-		return singleton.writer
+	if defaultLogger != nil {
+		return defaultLogger.writer
 	}
 	return nil
 }
 
-func Info(msg string, args ...any) {
-	if singleton != nil {
-		singleton.Info(msg, args...)
+// Info logs an informational message with optional arguments.
+func Info(msg string, args ...interface{}) {
+	if defaultLogger != nil {
+		defaultLogger.Info(msg, args...)
 	}
 }
 
-func Warn(msg string, args ...any) {
-	if singleton != nil {
-		singleton.Warn(msg, args...)
+// Warn logs a warning message with optional arguments.
+func Warn(msg string, args ...interface{}) {
+	if defaultLogger != nil {
+		defaultLogger.Warn(msg, args...)
 	}
 }
 
-func Error(msg string, args ...any) {
-	if singleton != nil {
-		singleton.Error(msg, args...)
+// Error logs an error message with optional arguments.
+func Error(msg string, args ...interface{}) {
+	if defaultLogger != nil {
+		defaultLogger.Error(msg, args...)
 	}
 }
 
-func Fatal(msg string, args ...any) {
-	if singleton != nil {
-		singleton.Error(msg, args...)
+// Fatal logs a fatal error message with optional arguments and exits the program.
+func Fatal(msg string, args ...interface{}) {
+	if defaultLogger != nil {
+		defaultLogger.Error(msg, args...)
 		os.Exit(1)
 	}
 }
 
-func Debug(msg string, args ...any) {
-	if singleton != nil {
-		singleton.Debug(msg, args...)
+// Debug logs a debug message with optional arguments.
+func Debug(msg string, args ...interface{}) {
+	if defaultLogger != nil {
+		defaultLogger.Debug(msg, args...)
 	}
 }
 
-func Log(level slog.Level, msg string, args ...any) {
-	if singleton != nil {
+// Log logs a message with the specified log level and optional arguments.
+func Log(level slog.Level, msg string, args ...interface{}) {
+	if defaultLogger != nil {
 		slevel := slog.Level(level)
-		singleton.Log(context.TODO(), slevel, msg, args...)
+		defaultLogger.Log(context.TODO(), slevel, msg, args...)
 	}
 }
